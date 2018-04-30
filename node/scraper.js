28ff6806028e3acb05a47abadbff194a1fcaa396
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-30 14:09:06 -0700

    + check for null

--- node/scraper.js
+++ node/scraper.js
@@ -4,10 +4,22 @@ const parser = require( "./parser" );
 ( async() => {
 	const browser = await pupp.launch();
 	const page = await browser.newPage();
-	await page.goto( "https://tabs.ultimate-guitar.com/tab/zac_brown_band/chicken_fried_tabs_761400" );
+	await page.goto( "https://tabs.ultimate-guitar.com/tab/eagles/hotel_california_tabs_14341" );
 
-	let difficulty = parser.difficulty( await page.evaluate( () => document.querySelector( "div._3JgI9" ).textContent ) );
-	let tuning = parser.tuning( await page.evaluate( () => document.querySelector( "#tuning" ).textContent ) );
+	let difficulty = parser.difficulty( await page.evaluate( () => { 
+			let ret = document.querySelector( "div._3JgI9" );
+
+			if ( ret ) {
+				return ret.textContent;
+			}
+		} ) );
+	let tuning = parser.tuning( await page.evaluate( () => {
+			let ret = document.querySelector( "#tuning" );
+
+			if ( ret ) {
+				return ret.textContent;
+			}
+		} ) );
 	let tabs = parser.tabs( await page.evaluate( () => window.UGAPP.store.page.data["tab_view"]["wiki_tab"].content ) );
 
 	console.log( difficulty, tuning );
