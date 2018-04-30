cc13a30fc8a83adc61ea28f7b5455140d8f15b6c
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-30 01:46:40 -0700

    + scraper

---
+++ node/scraper.js
@@ -0,0 +1,22 @@
+const pupp = require( "puppeteer" );
+const parser = require( "./parser" );
+
+( async() => {
+	const browser = await pupp.launch();
+	const page = await browser.newPage();
+	await page.goto( "https://tabs.ultimate-guitar.com/tab/zac_brown_band/chicken_fried_tabs_761400" );
+
+	let difficulty = parser.difficulty( await page.evaluate( () => document.querySelector( "div._3JgI9" ).textContent ) );
+	let tuning = parser.tuning( await page.evaluate( () => document.querySelector( "#tuning" ).textContent ) );
+	let tabs = parser.tabs( await page.evaluate( () => document.querySelector( "pre._1YgOS" ).textContent ) );
+
+	console.log( difficulty, tuning );
+	for ( let sec in tabs.sections ) {
+		let section = tabs.sections[sec];
+		console.log( sec, JSON.stringify( section ) );
+		console.log( "\n\n" );
+	}
+
+	browser.close();
+} )();
+
