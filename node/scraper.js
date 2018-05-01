5768abbfc375d0c595e4dbd55c0c5b1fb8971ceb
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-05-01 13:35:25 -0700

    + artist, song_name, tuning, tab_url, & difficulty direct extraction

--- node/scraper.js
+++ node/scraper.js
@@ -4,31 +4,37 @@ const parser = require( "./parser" );
 ( async() => {
 	const browser = await pupp.launch();
 	const page = await browser.newPage();
-	await page.goto( "https://tabs.ultimate-guitar.com/tab/eagles/hotel_california_tabs_14341" );
+	await page.goto( "https://tabs.ultimate-guitar.com/tab/zac_brown_band/chicken_fried_tabs_761400" );
 
-	let difficulty = parser.difficulty( await page.evaluate( () => { 
-			let ret = document.querySelector( "div._3JgI9" );
+	let song = await page.evaluate( () => { 
+			let tab_view = window.UGAPP.store.page.data.tab_view;
+			let tab = window.UGAPP.store.page.data.tab;
+			let tuning = tab_view.meta.tuning;
+			let difficulty = tab_view.meta.difficulty;
 
-			if ( ret ) {
-				return ret.textContent;
+			if  ( !tuning ) {
+				tuning = [ "E", "A", "D", "G", "B", "E" ];
+			} else {
+				tuning = tuning.value.split( " " );
 			}
-		} ) );
-	let tuning = parser.tuning( await page.evaluate( () => {
-			let ret = document.querySelector( "#tuning" );
 
-			if ( ret ) {
-				return ret.textContent;
+			if ( !difficulty ) {
+				difficulty = "unknown";
 			}
-		} ) );
-	let tabs = parser.tabs( await page.evaluate( () => window.UGAPP.store.page.data["tab_view"]["wiki_tab"].content ) );
 
-	console.log( difficulty, tuning );
-	for ( let sec in tabs.sections ) {
-		let section = tabs.sections[sec];
-		console.log( sec, JSON.stringify( section ) );
-		console.log( "\n\n" );
-	}
+			return {
+				artist: tab.artist_name,
+				song_name: tab.song_name,
+				tab_url: tab.tab_url,
+				difficulty: difficulty,
+				tuning: tuning,
+				tabs: tab_view.wiki_tab.content
+			}
+		} );
+	song.tabs = parser.tabs( song.tabs );
 
+	console.log( song );
+	console.log( JSON.stringify( song.tabs.sections["Verse 1"].measures[0] ) );
 	browser.close();
 } )();
 
