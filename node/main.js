ad1c309604c4fee15f38d7a37e961845b9d8be50
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-05-01 21:22:45 -0700

    + handles queries & sends back a list of items matching the search

--- node/main.js
+++ node/main.js
@@ -2,51 +2,34 @@ const index = require( "./ugs/index" );
 const scraper = require( "./scraper" );
 const http = require( "http" );
 
-/* function getBestTab( tabList ) {
-	let top = {
-		adv_rating: 0,
-		tab: null
-	};
-
-	let cur_adv;
-	let cur;
-	for ( let i = 0; i < tabList.length; i++ ) {
-		cur = tabList[i];
-		cur_adv = cur.rating * cur.numberRates;
-
-		if ( cur_adv > top.adv_rating ) {
-			top.adv_rating = cur_adv;
-			top.tab = cur;
-		}
-	}
-
-	return top.tab;
-}*/
-
-/*
-	index.search( { 
-				query: "chicken fried",  
-				page: 1, 
-				type: "Tab" 
-			}, ( a, b ) => console.log( getBestTab( b ) ) );
-*/
-
 http.createServer( function( request, response ) {
 	let formatted = formatRequest( request.url );
 	response.writeHead( 200, { 'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*' } ); // TODO add specific source
 
 	if ( formatted.type.toLowerCase() === "autocomplete" ) {
 		index.autocomplete( formatted.args.text, function( a, b ) {
-			if ( b && b.length > 0 ) {
+			if ( !a && b && b.length > 0 ) {
 				response.write( JSON.stringify( b ) )
 			}
 
 			response.end();
 		} );
+	} else if ( formatted.type.toLowerCase() === "query" ) {
+		if ( formatted.args.query ) {
+			index.search( formatted.args, function( a, b ) {
+				if ( !a && b ) {
+					response.write( JSON.stringify( b ) );
+				}
+
+				response.end();
+			} );
+		} else {
+			response.end();
+		}
 	} else {
 		response.end();
 	}
-	
+
 } ).listen( 8080 );
 
 function formatRequest( uri ) {
